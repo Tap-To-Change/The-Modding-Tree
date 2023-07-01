f71547fde04a19f1bd95e764d8ddbacad15bc2c9
KR Williams <krwill01@gmail.com>
2023-07-01 15:34:45 -0400

    Update mod.js
    Updated name, id, and author of the mod

--- js/mod.js
+++ js/mod.js
@@ -1,7 +1,7 @@
 let modInfo = {
-	name: "The ??? Tree",
-	id: "mymod",
-	author: "nobody",
+	name: "The Ultimate Tree",
+	id: "tut1000",
+	author: "Tap To Change",
 	pointsName: "points",
 	modFiles: ["layers.js", "tree.js"],
 
