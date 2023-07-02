aba3b91511ebe4b5c52cfc1282cc020974d9b012
KR Williams <krwill01@gmail.com>
2023-07-02 16:05:50 -0400

    Update layers.js
    yeah yeah yeah small rebalancing

--- js/layers.js
+++ js/layers.js
@@ -33,7 +33,7 @@ addLayer("rp", {
         description: "Increase point gain (based on RP).",
         cost: new Decimal(3),
         effect() {
-            return player[this.layer].points.add(1).pow(0.3)*2
+            return player[this.layer].points.add(1).pow(0.25)*2
         },
         effectDisplay() {return format(upgradeEffect(this.layer, this.id))+"x" }, // Add formatting to the effect 
     }
